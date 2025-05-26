bc9487e236914f14727f7ba4ab77ed0d19a8cf56
bTrung <nguyenviethoang040603@gmail.com>
2025-05-26 18:38:03 +0700

    fix realtime

--- air_quality_tracker/resources/views/layout.blade.php
+++ air_quality_tracker/resources/views/layout.blade.php
@@ -8,6 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Chart.js -->
     @vite('resources/js/app.js')
+    @vite('resources/js/bootstrap.js')
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
     <!-- Custom CSS -->
