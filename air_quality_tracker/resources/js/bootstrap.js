3a55143752e896ef5b87f4626fb431d766db68ba
bTrung <nguyenviethoang040603@gmail.com>
2025-06-03 18:18:09 +0700

    fix realtime private user

--- air_quality_tracker/resources/js/bootstrap.js
+++ air_quality_tracker/resources/js/bootstrap.js
@@ -35,7 +35,8 @@ window.Echo = new Echo({
 
 
 if (window.userId) {
-window.Echo.private('air-quality.${window.userId}')
+    const userId = window.userId;
+window.Echo.private('air-quality.${userId}')
     .listen('.AirQualityUpdated', (e) => {
         console.log('Realtime data:', e.data);
         // Giả sử e.data có dạng { temperature, humidity, PM2_5, CO, CO2, O3, TVOC }
