7078bf0073dfb1cb5906179d6f6a0f42433feb28
bTrung <nguyenviethoang040603@gmail.com>
2025-06-03 17:57:05 +0700

    fix realtime private user

--- air_quality_tracker/resources/js/bootstrap.js
+++ air_quality_tracker/resources/js/bootstrap.js
@@ -35,7 +35,7 @@ window.Echo = new Echo({
 
 
 if (window.laravel && window.laravel.userId) {
-window.Echo.private('air-quality.${userId}')
+window.Echo.private('air-quality.1')
     .listen('.AirQualityUpdated', (e) => {
         console.log('Realtime data:', e.data);
         // Giả sử e.data có dạng { temperature, humidity, PM2_5, CO, CO2, O3, TVOC }
